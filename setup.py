e32ccc3e59bf9034e8814897157653dde2a6a2e1
Eana Hufwe <ilove@1a23.com>
2018-01-10 09:25:02 +0800

    WIP 20180110, backup before migrating to entry_point structure

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ if sys.version_info < (3, 5):
     raise Exception("Python 3.5 or higher is required. Your version is %s." % sys.version)
 
 __version__ = ""
-exec(open('ehforwarderbot/channel/slave/blueset/wechat/__version__.py').read())
+exec(open('ehforwarderbot/channels/slave/blueset/wechat/__version__.py').read())
 
 setup(
-    name='ehforwarderbot-slave-wechat',
+    name='efb-wechat-slave',
     namespace_package=['ehforwarderbot'],
     version=__version__,
     description='WeChat Slave Channel for EH Forwarder Bot, based on WeChat Web API.',
