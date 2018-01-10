b791e93d10b2b3a3d5babcb82b95ab1a3deef713
Eana Hufwe <ilove@1a23.com>
2018-01-10 17:41:16 +0800

    (Might) release to PyPI as prerelease.

--- setup.py
+++ setup.py
@@ -5,19 +5,23 @@ if sys.version_info < (3, 5):
     raise Exception("Python 3.5 or higher is required. Your version is %s." % sys.version)
 
 __version__ = ""
-exec(open('ehforwarderbot/channels/slave/blueset/wechat/__version__.py').read())
+exec(open('efb_wechat_slave/__version__.py').read())
+
+long_description = open('README.rst').read()
 
 setup(
     name='efb-wechat-slave',
     namespace_package=['efb_wechat_slave'],
     version=__version__,
     description='WeChat Slave Channel for EH Forwarder Bot, based on WeChat Web API.',
+    long_description=long_description,
     author='Eana Hufwe',
     author_email='ilove@1a23.com',
     url='https://github.com/blueset/efb-telegram-master',
-    license='GPL v3',
+    license='GPLv3',
     download_url='',
-    keywords=['', ' '],
+    keywords=['ehforwarderbot', 'EH Forwarder Bot', 'EH Forwarder Bot Slave Channel',
+              'wechat', 'weixin', 'chatbot'],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
