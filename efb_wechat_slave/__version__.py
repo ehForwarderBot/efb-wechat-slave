ehForwarderBot/efb-wechat-slave

commit 608c1d6d4f3f9445d269d72af17c63dcd975ff7c

Add support to custom user agent string for Web WeChat. Fix #76.

efb_wechat_slave/__version__.py
@@ -1,3 +1,3 @@
 # coding: utf-8
 
-__version__ = "2.0.0a32.dev1"
+__version__ = "2.0.0a32.dev2"

efb_wechat_slave/vendor/itchat/components/login.py
@@ -91,7 +91,7 @@ def push_login(core):
     if 'wxuin' in cookiesDict:
         url = '%s/cgi-bin/mmwebwx-bin/webwxpushloginurl?uin=%s' % (
             config.BASE_URL, cookiesDict['wxuin'])
-        headers = {'User-Agent': config.USER_AGENT}
+        headers = {'User-Agent': self.user_agent}
         r = core.s.get(url, headers=headers).json()
         if 'uuid' in r and r.get('ret') in (0, '0'):
             core.uuid = r['uuid']
@@ -104,7 +104,7 @@ def get_QRuuid(self):
     params = {
         'appid': 'wx782c26e4c19acffb',
         'fun': 'new', }
-    headers = {'User-Agent': config.USER_AGENT}
+    headers = {'User-Agent': self.user_agent}
     r = self.s.get(url, params=params, headers=headers)
     regx = r'window.QRLogin.code = (\d+); window.QRLogin.uuid = "(\S+?)";'
     data = re.search(regx, r.text)
@@ -137,7 +137,7 @@ def check_login(self, uuid=None):
     localTime = int(time.time())
     params = 'loginicon=true&uuid=%s&tip=1&r=%s&_=%s' % (
         uuid, int(-localTime / 1579), localTime)
-    headers = {'User-Agent': config.USER_AGENT}
+    headers = {'User-Agent': self.user_agent}
     r = self.s.get(url, params=params, headers=headers)
     regx = r'window.code=(\d+)'
     data = re.search(regx, r.text)
@@ -160,7 +160,7 @@ def process_login_info(core, loginContent):
     """
     regx = r'window.redirect_uri="(\S+)";'
     core.loginInfo['url'] = re.search(regx, loginContent).group(1)
-    headers = {'User-Agent': config.USER_AGENT}
+    headers = {'User-Agent': self.user_agent}
     r = core.s.get(core.loginInfo['url'], headers=headers, allow_redirects=False)
     core.loginInfo['url'] = core.loginInfo['url'][:core.loginInfo['url'].rfind('/')]
     for indexUrl, detailedUrl in (
@@ -203,7 +203,7 @@ def web_init(self):
     data = {'BaseRequest': self.loginInfo['BaseRequest'], }
     headers = {
         'ContentType': 'application/json; charset=UTF-8',
-        'User-Agent': config.USER_AGENT, }
+        'User-Agent': self.user_agent, }
     r = self.s.post(url, params=params, data=json.dumps(data), headers=headers)
     dic = json.loads(r.content.decode('utf-8', 'replace'))
     # deal with login info
@@ -246,7 +246,7 @@ def show_mobile_login(self):
         'ClientMsgId': int(time.time()), }
     headers = {
         'ContentType': 'application/json; charset=UTF-8',
-        'User-Agent': config.USER_AGENT, }
+        'User-Agent': self.user_agent, }
     r = self.s.post(url, data=json.dumps(data), headers=headers)
     return ReturnValue(rawResponse=r)
 
@@ -314,7 +314,7 @@ def sync_check(self):
         'deviceid': self.loginInfo['deviceid'],
         'synckey': self.loginInfo['synckey'],
         '_': self.loginInfo['logintime'], }
-    headers = {'User-Agent': config.USER_AGENT}
+    headers = {'User-Agent': self.user_agent}
     self.loginInfo['logintime'] += 1
     try:
         r = self.s.get(url, params=params, headers=headers, timeout=config.TIMEOUT)
@@ -348,7 +348,7 @@ def get_msg(self):
         'rr': ~int(time.time()), }
     headers = {
         'ContentType': 'application/json; charset=UTF-8',
-        'User-Agent': config.USER_AGENT}
+        'User-Agent': self.user_agent}
     r = self.s.post(url, data=json.dumps(data), headers=headers, timeout=config.TIMEOUT)
     dic = json.loads(r.content.decode('utf-8', 'replace'))
     if dic['BaseResponse']['Ret'] != 0: return None, None
@@ -365,7 +365,7 @@ def logout(self):
             'redirect': 1,
             'type': 1,
             'skey': self.loginInfo['skey'], }
-        headers = {'User-Agent': config.USER_AGENT}
+        headers = {'User-Agent': self.user_agent}
         self.s.get(url, params=params, headers=headers)
         self.alive = False
     self.isLogging = False

efb_wechat_slave/vendor/itchat/components/messages.py
@@ -30,7 +30,7 @@ def get_download_fn(core, url, msgId):
         params = {
             'msgid': msgId,
             'skey': core.loginInfo['skey'], }
-        headers = {'User-Agent': config.USER_AGENT}
+        headers = {'User-Agent': self.user_agent}
         r = core.s.get(url, params=params, stream=True, headers=headers)
         tempStorage = io.BytesIO()
         for block in r.iter_content(1024):
@@ -129,7 +129,7 @@ def produce_msg(core, msgList):
                 params = {
                     'msgid': msgId,
                     'skey': core.loginInfo['skey'], }
-                headers = {'Range': 'bytes=0-', 'User-Agent': config.USER_AGENT}
+                headers = {'Range': 'bytes=0-', 'User-Agent': self.user_agent}
                 r = core.s.get(url, params=params, headers=headers, stream=True)
                 tempStorage = io.BytesIO()
                 for block in r.iter_content(1024):
@@ -162,7 +162,7 @@ def produce_msg(core, msgList):
                     'fromuser': core.loginInfo['wxuin'],
                     'pass_ticket': 'undefined',
                     'webwx_data_ticket': cookiesList['webwx_data_ticket'], }
-                headers = {'User-Agent': config.USER_AGENT}
+                headers = {'User-Agent': self.user_agent}
 
                 def download_atta(attaDir=None):
                     r = core.s.get(url, params=params, stream=True, headers=headers)
@@ -283,7 +283,7 @@ def send_raw_msg(self, msgType, content, toUserName):
             'ClientMsgId': int(time.time() * 1e4),
         },
         'Scene': 0, }
-    headers = {'ContentType': 'application/json; charset=UTF-8', 'User-Agent': config.USER_AGENT}
+    headers = {'ContentType': 'application/json; charset=UTF-8', 'User-Agent': self.user_agent}
     r = self.s.post(url, headers=headers,
                     data=json.dumps(data, ensure_ascii=False).encode('utf8'))
     return ReturnValue(rawResponse=r)
@@ -378,7 +378,7 @@ def upload_chunk_file(core, fileDir, fileSymbol, fileSize,
         del files['chunks']
     else:
         files['chunk'], files['chunks'] = (None, str(chunk)), (None, str(chunks))
-    headers = {'User-Agent': config.USER_AGENT}
+    headers = {'User-Agent': self.user_agent}
     return core.s.post(url, files=files, headers=headers, timeout=config.TIMEOUT)
 
 
@@ -417,7 +417,7 @@ def send_file(self, fileDir, toUserName=None, mediaId=None, file_=None):
             'ClientMsgId': int(time.time() * 1e4), },
         'Scene': 0, }
     headers = {
-        'User-Agent': config.USER_AGENT,
+        'User-Agent': self.user_agent,
         'Content-Type': 'application/json;charset=UTF-8', }
     r = self.s.post(url, headers=headers,
                     data=json.dumps(data, ensure_ascii=False).encode('utf8'))
@@ -460,7 +460,7 @@ def send_image(self, fileDir=None, toUserName=None, mediaId=None, file_=None):
         data['Msg']['Type'] = 47
         data['Msg']['EmojiFlag'] = 2
     headers = {
-        'User-Agent': config.USER_AGENT,
+        'User-Agent': self.user_agent,
         'Content-Type': 'application/json;charset=UTF-8', }
     r = self.s.post(url, headers=headers,
                     data=json.dumps(data, ensure_ascii=False).encode('utf8'))
@@ -500,7 +500,7 @@ def send_video(self, fileDir=None, toUserName=None, mediaId=None, file_=None):
             'ClientMsgId': int(time.time() * 1e4), },
         'Scene': 0, }
     headers = {
-        'User-Agent': config.USER_AGENT,
+        'User-Agent': self.user_agent,
         'Content-Type': 'application/json;charset=UTF-8', }
     r = self.s.post(url, headers=headers,
                     data=json.dumps(data, ensure_ascii=False).encode('utf8'))
@@ -543,7 +543,7 @@ def revoke(self, msgId, toUserName, localId=None):
         "ToUserName": toUserName}
     headers = {
         'ContentType': 'application/json; charset=UTF-8',
-        'User-Agent': config.USER_AGENT}
+        'User-Agent': self.user_agent}
     r = self.s.post(url, headers=headers,
                     data=json.dumps(data, ensure_ascii=False).encode('utf8'))
     return ReturnValue(rawResponse=r)

efb_wechat_slave/vendor/wxpy/api/bot.py
@@ -49,7 +49,8 @@ class Bot(object):
 
     def __init__(
             self, cache_path=None, console_qr=False, qr_path=None,
-            qr_callback=None, login_callback=None, logout_callback=None
+            qr_callback=None, login_callback=None, logout_callback=None,
+            user_agent=None
     ):
         """
         :param cache_path:
@@ -65,9 +66,11 @@ class Bot(object):
         :param qr_callback: 获得二维码后的回调，可以用来定义二维码的处理方式，接收参数: uuid, status, qrcode
         :param login_callback: 登陆成功后的回调，若不指定，将进行清屏操作，并删除二维码文件
         :param logout_callback: 登出时的回调
+        :param user_agent: User agent used during request.
         """
 
-        self.core = itchat.Core()
+        self.core = itchat.Core(user_agent)
+        self.user_agent = self.core.user_agent
         itchat.instanceList.append(self)
 
         enhance_connection(self.core.s)

efb_wechat_slave/__init__.py
@@ -541,7 +541,8 @@ class WeChatChannel(EFBChannel):
         with coordinator.mutex:
             self.bot: wxpy.Bot = wxpy.Bot(cache_path=str(efb_utils.get_data_path(self.channel_id) / "wxpy.pkl"),
                                           qr_callback=qr_callback,
-                                          logout_callback=self.exit_callback)
+                                          logout_callback=self.exit_callback,
+                                          user_agent=self.flag('user_agent'))
             self.bot.enable_puid(
                 efb_utils.get_data_path(self.channel_id) / "wxpy_puid.pkl",
                 self.flag('puid_logs')

efb_wechat_slave/slave_message.py
@@ -430,7 +430,7 @@ class SlaveMessageManager:
                     'msgid': msg.id,
                     'skey': msg.bot.core.loginInfo['skey']
                 }
-                headers = {'User-Agent': itchat.config.USER_AGENT}
+                headers = {'User-Agent': self.bot.user_agent}
                 if app_message == 'thumbnail':
                     params['type'] = 'slave'
                 r = msg.bot.core.s.get(url, params=params, stream=True, headers=headers)

efb_wechat_slave/utils.py
@@ -96,6 +96,7 @@ WC_EMOTICON_CONVERSION = {
     '[红包]': '💰', '[Packet]': '💰',
     '[鸡]': '🐥', '[Chick]': '🐥',
     '[蜡烛]': '🕯️', '[Candle]': '🕯️',
+    '[糗大了]': '😥',
     '[Thumbs Up]': '👍', '[Pleased]': '😊',
     '[Rich]': '🀅',
     '[Pup]': '🐶',
@@ -114,7 +115,8 @@ class ExperimentalFlagsManager:
         'app_shared_link_mode': 'ignore',
         'puid_logs': None,
         'send_stickers_and_gif_as_jpeg': False,
-        'system_chats_to_include': ['filehelper']
+        'system_chats_to_include': ['filehelper'],
+        'user_agent': None
     }
 
     def __init__(self, channel: 'WeChatChannel'):

efb_wechat_slave/vendor/itchat/components/contact.py
@@ -37,7 +37,7 @@ def update_chatroom(self, userName, detailedMember=False):
         self.loginInfo['url'], int(time.time()))
     headers = {
         'ContentType': 'application/json; charset=UTF-8',
-        'User-Agent': config.USER_AGENT}
+        'User-Agent': self.user_agent}
     data = {
         'BaseRequest': self.loginInfo['BaseRequest'],
         'Count': len(userName),
@@ -57,7 +57,7 @@ def update_chatroom(self, userName, detailedMember=False):
                 self.loginInfo['url'], int(time.time()))
             headers = {
                 'ContentType': 'application/json; charset=UTF-8',
-                'User-Agent': config.USER_AGENT, }
+                'User-Agent': self.user_agent, }
             data = {
                 'BaseRequest': self.loginInfo['BaseRequest'],
                 'Count': len(memberList),
@@ -89,7 +89,7 @@ def update_friend(self, userName):
         self.loginInfo['url'], int(time.time()))
     headers = {
         'ContentType': 'application/json; charset=UTF-8',
-        'User-Agent': config.USER_AGENT}
+        'User-Agent': self.user_agent}
     data = {
         'BaseRequest': self.loginInfo['BaseRequest'],
         'Count': len(userName),
@@ -281,7 +281,7 @@ def get_contact(self, update=False):
                                                           int(time.time()), seq, self.loginInfo['skey'])
         headers = {
             'ContentType': 'application/json; charset=UTF-8',
-            'User-Agent': config.USER_AGENT, }
+            'User-Agent': self.user_agent, }
         try:
             r = self.s.get(url, headers=headers)
         except:
@@ -347,7 +347,7 @@ def set_alias(self, userName, alias):
         'CmdId': 2,
         'RemarkName': alias,
         'BaseRequest': self.loginInfo['BaseRequest'], }
-    headers = {'User-Agent': config.USER_AGENT}
+    headers = {'User-Agent': self.user_agent}
     r = self.s.post(url, json.dumps(data, ensure_ascii=False).encode('utf8'),
                     headers=headers)
     r = ReturnValue(rawResponse=r)
@@ -364,7 +364,7 @@ def set_pinned(self, userName, isPinned=True):
         'CmdId': 3,
         'OP': int(isPinned),
         'BaseRequest': self.loginInfo['BaseRequest'], }
-    headers = {'User-Agent': config.USER_AGENT}
+    headers = {'User-Agent': self.user_agent}
     r = self.s.post(url, json=data, headers=headers)
     return ReturnValue(rawResponse=r)
 
@@ -389,7 +389,7 @@ def add_friend(self, userName, status=2, verifyContent='', autoUpdate=True):
         'skey': self.loginInfo['skey'], }
     headers = {
         'ContentType': 'application/json; charset=UTF-8',
-        'User-Agent': config.USER_AGENT}
+        'User-Agent': self.user_agent}
     r = self.s.post(url, headers=headers,
                     data=json.dumps(data, ensure_ascii=False).encode('utf8', 'replace'))
     if autoUpdate:
@@ -427,7 +427,7 @@ def get_head_img(self, userName=None, chatroomUserName=None, picDir=None):
             if 'EncryChatRoomId' in chatroom:
                 params['chatroomid'] = chatroom['EncryChatRoomId']
             params['chatroomid'] = params.get('chatroomid') or chatroom['UserName']
-    headers = {'User-Agent': config.USER_AGENT}
+    headers = {'User-Agent': self.user_agent}
     r = self.s.get(url, params=params, stream=True, headers=headers)
     tempStorage = io.BytesIO()
     for block in r.iter_content(1024):
@@ -453,7 +453,7 @@ def create_chatroom(self, memberList, topic=''):
         'Topic': topic, }
     headers = {
         'content-type': 'application/json; charset=UTF-8',
-        'User-Agent': config.USER_AGENT}
+        'User-Agent': self.user_agent}
     r = self.s.post(url, headers=headers,
                     data=json.dumps(data, ensure_ascii=False).encode('utf8', 'ignore'))
     return ReturnValue(rawResponse=r)
@@ -468,7 +468,7 @@ def set_chatroom_name(self, chatroomUserName, name):
         'NewTopic': name, }
     headers = {
         'content-type': 'application/json; charset=UTF-8',
-        'User-Agent': config.USER_AGENT}
+        'User-Agent': self.user_agent}
     r = self.s.post(url, headers=headers,
                     data=json.dumps(data, ensure_ascii=False).encode('utf8', 'ignore'))
     return ReturnValue(rawResponse=r)
@@ -483,7 +483,7 @@ def delete_member_from_chatroom(self, chatroomUserName, memberList):
         'DelMemberList': ','.join([member['UserName'] for member in memberList]), }
     headers = {
         'content-type': 'application/json; charset=UTF-8',
-        'User-Agent': config.USER_AGENT}
+        'User-Agent': self.user_agent}
     r = self.s.post(url, data=json.dumps(data), headers=headers)
     return ReturnValue(rawResponse=r)
 
@@ -512,6 +512,6 @@ def add_member_into_chatroom(self, chatroomUserName, memberList,
         memberKeyName: ','.join([member['UserName'] for member in memberList]), }
     headers = {
         'content-type': 'application/json; charset=UTF-8',
-        'User-Agent': config.USER_AGENT}
+        'User-Agent': self.user_agent}
     r = self.s.post(url, data=json.dumps(params), headers=headers)
     return ReturnValue(rawResponse=r)

commit 0e3256311d9f43c5556fcfc8cb6c7bde00b5cc59

Fix name resolution bug caused by 608c1d6.

efb_wechat_slave/__version__.py
@@ -1,3 +1,3 @@
 # coding: utf-8
 
-__version__ = "2.0.0a32.dev2"
+__version__ = "2.0.0a32.dev3"

efb_wechat_slave/vendor/itchat/components/login.py
@@ -91,7 +91,7 @@ def push_login(core):
     if 'wxuin' in cookiesDict:
         url = '%s/cgi-bin/mmwebwx-bin/webwxpushloginurl?uin=%s' % (
             config.BASE_URL, cookiesDict['wxuin'])
-        headers = {'User-Agent': self.user_agent}
+        headers = {'User-Agent': core.user_agent}
         r = core.s.get(url, headers=headers).json()
         if 'uuid' in r and r.get('ret') in (0, '0'):
             core.uuid = r['uuid']
@@ -160,7 +160,7 @@ def process_login_info(core, loginContent):
     """
     regx = r'window.redirect_uri="(\S+)";'
     core.loginInfo['url'] = re.search(regx, loginContent).group(1)
-    headers = {'User-Agent': self.user_agent}
+    headers = {'User-Agent': core.user_agent}
     r = core.s.get(core.loginInfo['url'], headers=headers, allow_redirects=False)
     core.loginInfo['url'] = core.loginInfo['url'][:core.loginInfo['url'].rfind('/')]
     for indexUrl, detailedUrl in (

efb_wechat_slave/vendor/itchat/components/messages.py
@@ -30,7 +30,7 @@ def get_download_fn(core, url, msgId):
         params = {
             'msgid': msgId,
             'skey': core.loginInfo['skey'], }
-        headers = {'User-Agent': self.user_agent}
+        headers = {'User-Agent': core.user_agent}
         r = core.s.get(url, params=params, stream=True, headers=headers)
         tempStorage = io.BytesIO()
         for block in r.iter_content(1024):
@@ -129,7 +129,7 @@ def produce_msg(core, msgList):
                 params = {
                     'msgid': msgId,
                     'skey': core.loginInfo['skey'], }
-                headers = {'Range': 'bytes=0-', 'User-Agent': self.user_agent}
+                headers = {'Range': 'bytes=0-', 'User-Agent': core.user_agent}
                 r = core.s.get(url, params=params, headers=headers, stream=True)
                 tempStorage = io.BytesIO()
                 for block in r.iter_content(1024):
@@ -162,7 +162,7 @@ def produce_msg(core, msgList):
                     'fromuser': core.loginInfo['wxuin'],
                     'pass_ticket': 'undefined',
                     'webwx_data_ticket': cookiesList['webwx_data_ticket'], }
-                headers = {'User-Agent': self.user_agent}
+                headers = {'User-Agent': core.user_agent}
 
                 def download_atta(attaDir=None):
                     r = core.s.get(url, params=params, stream=True, headers=headers)
@@ -378,7 +378,7 @@ def upload_chunk_file(core, fileDir, fileSymbol, fileSize,
         del files['chunks']
     else:
         files['chunk'], files['chunks'] = (None, str(chunk)), (None, str(chunks))
-    headers = {'User-Agent': self.user_agent}
+    headers = {'User-Agent': core.user_agent}
     return core.s.post(url, files=files, headers=headers, timeout=config.TIMEOUT)
 
 

efb_wechat_slave/vendor/wxpy/api/bot.py
@@ -403,7 +403,7 @@ class Bot(object):
         if user_name:
             return Group(self.core.update_chatroom(userName=user_name), self)
         else:
-            from ...utils import decode_text_from_webwx
+            from ..utils import decode_text_from_webwx
             ret = decode_text_from_webwx(pformat(ret))
             raise Exception('Failed to create group:\n{}'.format(ret))
 