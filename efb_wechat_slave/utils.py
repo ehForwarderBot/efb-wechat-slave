fc9f1000f94447a0a5e33fda2ad043a8b0c3d50b
realdeveloperongithub <70182691+realdeveloperongithub@users.noreply.github.com>
2022-07-25 20:45:33 +0800

    Update utils.py

--- efb_wechat_slave/utils.py
+++ efb_wechat_slave/utils.py
@@ -150,7 +150,7 @@ class ExperimentalFlagsManager:
         return self.config[flag_key]
 
 
-def wechat_string_unescape(content: str) -> str:
+def wechat_string_unescape(content: str, replace_emoticon: bool = True) -> str:
     """
     Unescape a WeChat HTML string.
 
@@ -164,8 +164,9 @@ def wechat_string_unescape(content: str) -> str:
         return ""
     d: Dict[str, Any] = {"Content": content}
     itchat_utils.msg_formatter(d, "Content")
-    for i in WC_EMOTICON_CONVERSION:
-        d['Content'] = d['Content'].replace(i, WC_EMOTICON_CONVERSION[i])
+    if replace_emoticon:
+        for i in WC_EMOTICON_CONVERSION:
+            d['Content'] = d['Content'].replace(i, WC_EMOTICON_CONVERSION[i])
     return d['Content']
 
 
